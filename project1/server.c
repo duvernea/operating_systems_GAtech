f77f30e09495973d3b3206d4340b14f3899c508f
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-04-01 22:15:36 -0500

    feat: Add setup vars and argc checking to server.c

--- project1/server.c
+++ project1/server.c
@@ -8,6 +8,26 @@ void error(char *msg);
 
 
 int main(int argc, char* argv[]) {
+	int sockfd;		// return from socket system call
+	int newsockfd;	// return from accept system call
+	int portno;		
+	int clilen;		// size of the address of the client (needed for the accept system call)
+	int n;			// return value from read() and write(). #chars read or writen
+	
+
+    char buffer[256];
+	// The server reads characters from the socket connection into this buffer.
+	// The server code has a server and client sockaddr_in:
+	struct sockaddr_in serv_addr, cli_addr;
+
+	// The user needs to pass in the port number on which the server 
+	// will accept connections as an argument. 
+	// This code displays an error message if the user fails to do this.
+	if (argc < 2) {
+		fprintf(stderr,"ERROR, no port provided\n");
+		exit(1);
+     }
+
 	return 0;
 }
 
