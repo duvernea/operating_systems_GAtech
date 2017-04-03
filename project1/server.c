ae6727db1d6886b7044e705371890e70114cbbff
BRIAN DUVERNEAY <duverneay@gmail.com>
2017-04-02 20:40:38 -0500

    feat: Add accept() to allow connection of clients
    At this point when running the server, I am unable to connect my client. 'ERROR conection: Connection refused.' Need to debug

--- project1/server.c
+++ project1/server.c
@@ -3,15 +3,20 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
 
 void error(char *msg);
 
 
 int main(int argc, char* argv[]) {
+	int set_reuse_addr = 1; // ON == 1  
 	int sockfd;		// return from socket system call
 	int newsockfd;	// return from accept system call
 	int portno;		
-	int clilen;		// size of the address of the client (needed for the accept system call)
+	// int clilen;		// size of the address of the client (needed for the accept system call)
+	socklen_t clilen;
 	int n;			// return value from read() and write(). #chars read or writen
 	
 
@@ -32,6 +37,12 @@ int main(int argc, char* argv[]) {
 	if (sockfd < 0) {
 		error("ERROR opening socket");
 	}
+// Set socket to use wildcards - i.e. 0.0.0.0:21 and 192.168.0.1:21
+  // can be bound separately (helps to avoid conflicts) 
+  if (0 != setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &set_reuse_addr, sizeof(set_reuse_addr))) {
+    fprintf(stderr, "server failed to set SO_REUSEADDR socket option (not fatal)\n");
+  }
+
 	// set all values to zero
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	// the port number on which the server will listen for connections
@@ -41,6 +52,18 @@ int main(int argc, char* argv[]) {
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(portno);
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+	// struct sockaddr_in sa;
+	// char str[INET_ADDRSTRLEN];
+
+	// // store this IP address in sa:
+	// inet_pton(AF_INET, "192.0.2.33", &(sa.sin_addr));
+
+	// // now get it back and print it
+	// inet_ntop(AF_INET, &(sa.sin_addr), str, INET_ADDRSTRLEN);
+
+	// printf("%s\n", str); // prints "192.0.2.33"
+
 	// bind socket to address
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
 		error("ERROR on binding");
@@ -48,6 +71,27 @@ int main(int argc, char* argv[]) {
 	// 5 = size of the backlog queue, #connections that can be waiting 
 	// while the process is handling a particular connection. 5=max size permitted by most systems
 	listen(sockfd,5);
+
+	clilen = sizeof(cli_addr);
+	// The accept() system call causes the process to block until a client 
+	// connects to the server. Thus, it wakes up the process when a connection 
+	// from a client has been successfully established. It returns a new file
+	// descriptor, and all communication on this connection should be done 
+	// using the new file descriptor. The second argument is a reference 
+	// pointer to the address of the client on the other end of the connection, 
+	// and the third argument is the size of this structure.
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	if (newsockfd < 0) {
+		error("ERROR on accept");
+	}
+	bzero(buffer,256);
+	n = read(newsockfd,buffer,255);
+	if (n < 0) error("ERROR reading from socket");
+	printf("Here is the message: %s\n",buffer);
+	n = write(newsockfd,"I got your message",18);
+	if (n < 0) error("ERROR writing to socket");
+	close(newsockfd);
+	close(sockfd);
 
 	return 0;
 }
